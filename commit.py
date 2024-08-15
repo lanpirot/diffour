31c0965674b5b70ad3314cdc54450fee75b122be
Alexander Boll <alexander.boll@unibe.ch>
2024-08-15 15:43:20 +0200

    test_commit done

--- commit.py
+++ commit.py
@@ -8,7 +8,7 @@ bit_mask_length = 64
 all_ones = 2**bit_mask_length - 1
 cherry_commit_message_pattern = r"\(cherry picked from commit ([a-fA-F0-9]{40})\)"
 git_origin_pattern = r"GitOrigin-RevId: ([a-fA-F0-9]{40})"
-date_id = 0  #the cherry picks are sorted by date, we give them an ID by our processing order
+
 
 
 def rotate_left(bitmask):
@@ -49,25 +49,33 @@ def mingle_shingles(wdiff, n):
 
 #the unidiff lib cuts off the leading - and + of the hunk body
 #add it back in to not confuse "- print()" with "+ print()"
-def get_hunk_strings(hunk, w_context, w_body):
+def get_hunk_strings(hunk, w_context, w_body, rs):
     ret = []
     for line in hunk:
-        l = line.value.rstrip()
+        l = line.value
+        if rs:
+            l = l.rstrip()
         if line.is_context:
             ret.append((l, w_context))
         elif line.is_added:
             ret.append(("+"+l, w_body))
         elif line.is_removed:
             ret.append(("-"+l, w_body))
+        elif line.value == ' No newline at end of file\n':
+            ret.append((l, w_context))
         else:
             raise
     return ret
 
 class Commit:
+    date_id = 2 ** bit_mask_length  # the cherry picks are sorted by date, we give them an ID by our processing order
+    normal_weight = 1
+    special_weight = 10
+    rs = True
+
     def __init__(self, commit_str, diff_marker):
-        global date_id
-        self.date = date_id
-        date_id += 1
+        self.date = self.__class__.date_id
+        self.__class__.date_id -= 1
 
         self.parent_id = None
         self.is_root = None
@@ -117,7 +125,7 @@ class Commit:
         self.claimed_cherries = self.get_claimed_cherries()
 
     def has_rev_id(self):
-        return re.search(git_origin_pattern, self.commit_message)
+        return re.search(git_origin_pattern, self.commit_message) is not None
 
     def get_rev_id(self):
         if not self.has_rev_id():
@@ -126,11 +134,11 @@ class Commit:
 
     # does the commit message claim it was a cherry pick?
     def claims_cherry_pick(self):
-        return re.search(cherry_commit_message_pattern, self.commit_message)
+        return re.search(cherry_commit_message_pattern, self.commit_message) is not None
 
     def get_claimed_cherries(self):
         if not self.claims_cherry_pick():
-            return None
+            return []
         matches = re.findall(cherry_commit_message_pattern, self.commit_message)
         return list(matches)
 
@@ -140,24 +148,18 @@ class Commit:
         return False
 
     def get_ordered_commit_pair(self, other_commit):
-        if self.date > other_commit.date:
+        if self.date < other_commit.date:
             return self, other_commit
         return other_commit, self
 
     # a list of weighted strings, the strings are mostly the diff itself
     # <string>, <weight>
-    # commit_message, 1
-    # file_before, file_after 10
-    # hunk_header 1
-    # context_before 1
-    # body, 10
-    # context_after 1
     def get_weighted_diff(self):
-        w_message = 1
-        w_filename = 10
-        w_hheader = 1
-        w_context = 1
-        w_body = 10
+        w_message = self.__class__.normal_weight
+        w_filename = self.__class__.special_weight
+        w_hheader = self.__class__.normal_weight
+        w_context = self.__class__.normal_weight
+        w_body = self.__class__.special_weight
 
         if not self.parseable:
             return None
@@ -167,9 +169,8 @@ class Commit:
         for patch in self.patch_set:
             weighted_diff += [(patch.source_file, w_filename), (patch.target_file, w_filename)]
             for hunk in patch:
-                header = [(",".join([str(i) for i in [hunk.source_start, hunk.source_length, hunk.target_start,
-                                                      hunk.target_length]]), w_hheader)]
-                body = get_hunk_strings(hunk, w_context, w_body)
+                header = [(",".join([str(i) for i in [hunk.source_start, hunk.source_length, hunk.target_start, hunk.target_length]]), w_hheader)]
+                body = get_hunk_strings(hunk, w_context, w_body, self.__class__.rs)
                 weighted_diff += header
                 weighted_diff += body
                 weighted_diff += mingle_shingles(body, 2)
@@ -178,11 +179,9 @@ class Commit:
     # create a bit_mask (a signature) of a commit
     def get_bit_mask(self):
         wdiff = self.get_weighted_diff()
-        #wdiff = right_strip(wdiff)
 
         sim_hash_sum = np.zeros(bit_mask_length)
         for (line, weight) in wdiff:
-            #hsh = hash(line)
             mask = (1 << bit_mask_length) - 1
             hsh = mmh3.hash128(line) & mask
 
